d635088e6d21e0ad40d09ca66ca9d756226b8afb
CYBERSAMURA1 <CYBERSAMURA1@users.noreply.github.com>
2019-07-02 18:19:54 +0400

    осталось одно активити, цвета меняются рандомно

--- app/src/main/java/com/example/miha1/MainActivity.java
+++ app/src/main/java/com/example/miha1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.miha1;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,33 +10,89 @@ import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.Random;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton btnNextPage;
+    String KEY_NUMBER = "number";
 
+    ImageButton btnNextPage, btnPrevPage;
+
+    TextView pageNumber;
+
+    RelativeLayout background;
+
+    int a = 1;
+
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnNextPage = (ImageButton) findViewById(R.id.next);
 
-        TextView number = (TextView)findViewById(R.id.number);
-        number.setText("1");
+        btnNextPage = findViewById(R.id.next);
+        btnPrevPage = findViewById(R.id.prev);
 
-        RelativeLayout background = (RelativeLayout)findViewById(R.id.background);
-        background.setBackgroundColor(getResources().getColor(R.color.frst));
-
-        ImageButton btnPrevPage =(ImageButton)findViewById(R.id.prev);
-        btnPrevPage.setVisibility(View.INVISIBLE);
+        pageNumber = (TextView) findViewById(R.id.number);
 
 
-        btnNextPage.setOnClickListener(new View.OnClickListener(){
+        final int keyNumber = getIntent().getIntExtra(KEY_NUMBER, 1); //здесь ты должен тоже получить инт по ключу KEY_NUMBER
+        //это значение будет в том случае если  getIntent().getIntExtra(KEY_NUMBER,1) будет нулл , тоесть при первом запуске активити будет нулл
+        // тк мы еще никуда не нажимали чтоб получать даннык
+        a = keyNumber;
+
+        pageNumber.setText(String.format("%d", keyNumber));
+
+//теперь тебе надо получить данные по ключу ключ у тебя "number" - это константа лучше вынеси ее
+
+//       name  - String
+
+
+        background = findViewById(R.id.background);
+        background.setBackgroundColor(getRandBackgroundColor());
+
+        if (keyNumber == 1) {
+            btnPrevPage.setVisibility(View.INVISIBLE);
+        }
+
+        if (keyNumber == 7) {
+            btnNextPage.setVisibility(View.INVISIBLE);
+        }
+
+        btnNextPage.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this,Activity2.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                if (keyNumber <= 6) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    intent.putExtra(KEY_NUMBER, a + 1);//вот здесь ты ложишь в интент значение а+1 - эот интейджр
+                    startActivity(intent);
+                }
+
             }
         });
+
+        btnPrevPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        background.setBackgroundColor(getRandBackgroundColor());
+    }
+
+
+    private int getRandBackgroundColor() {
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+
+        return color;
     }
 }
 
